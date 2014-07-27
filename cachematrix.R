e4d913ac1bbd0242e53f5e5288e1c5a87142a7bb
manelfp <manelrallye@yahoo.es>
2014-07-27 16:29:34 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+# We want to solve XY = Id, in which case Y is the inverse of the matrix X.
+# Let X square matrix with nonzero determinant.
+# Set X=(xij) X=(yij) Id=Kronecker delta (1 when i=j, 0 otherwise)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+	#Sorry, but I have no time. Greetings from Barcelona, Manel.
 }
 
+# Algorithm
+# solve sum by j of: xij*yjk=Kronecker delta(i,k)
+# this is what makes the solve function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+      ## Return a matrix that is the inverse of 'x'
+	inverse <- solve(x)
+	return(inverse)
 }
